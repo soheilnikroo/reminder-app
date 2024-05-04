bfea0bd6bc609955588012277823fec330eb5242
Soheil Nikroo <soheiln1234@gmail.com>
2024-05-04 14:15:20 +0330

    Initial Commit

---
+++ reminder-practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reminder-practice
+//
+//  Created by Soheil Nikroo on 5/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
